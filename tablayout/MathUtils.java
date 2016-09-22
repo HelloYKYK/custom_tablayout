d0d6d76aab52ba127b6ba038627e9e44317b8e34
zhaohan <tstsolykyk@163.com>
2016-09-22 10:17:22 +0800

    first commit

---
+++ tablayout/MathUtils.java
@@ -0,0 +1,12 @@
+package com.okoer.ui.widget.tablayout;
+
+class MathUtils {
+
+    static int constrain(int amount, int low, int high) {
+        return amount < low ? low : (amount > high ? high : amount);
+    }
+
+    static float constrain(float amount, float low, float high) {
+        return amount < low ? low : (amount > high ? high : amount);
+    }
+}
